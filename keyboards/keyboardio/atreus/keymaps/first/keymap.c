d4b44c227e7e505e5adc6b0a5bac9f7965b2374c
Alexander Larsen <alexnlarsen@gmail.com>
2022-09-16 13:35:57 +0200

    Remove layout toggle

--- keyboards/keyboardio/atreus/keymaps/first/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/first/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_HOME, KC_7,    KC_8,   KC_9, KC_PIPE,
     KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_END,  KC_4,    KC_5,   KC_6, KC_BSLS,
     KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
+    KC_ESC, KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
   /*
    * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
    *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
@@ -41,6 +41,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ALGR(KC_Z),        ALGR(KC_L),       ALGR(KC_W),       KC_NO,      QK_BOOT, _______,    _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
     KC_NO,             KC_VOLD,          KC_LGUI,          KC_LSFT,    KC_BSPC, KC_LCTL,    KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
 };
-
-
 
