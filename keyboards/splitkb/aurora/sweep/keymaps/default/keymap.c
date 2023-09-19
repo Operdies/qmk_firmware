d993669951ff50f74208f762d94d9f88126e2a70
alex <alexnlarsen@gmail.com>
2023-09-19 20:20:11 +0200

    Stuff and things

--- keyboards/splitkb/aurora/sweep/keymaps/default/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/default/keymap.c
@@ -57,52 +57,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_BSPC      , LT(MO(_SYMBOLS), KC_TAB) ,     LT(MO(_MOUSE), KC_ENT) , KC_SPC
 ),
 
-//    ┌───┬──────────────┬──────────────┬──────────────┬───────────┐                              ┌──────┬───────────┬───────────┬───────────┬───┐
-//    │ ` │      =       │      -       │      '       │   C(\)    │                              │  no  │     7     │     8     │     9     │ | │
-//    ├───┼──────────────┼──────────────┼──────────────┼───────────┤                              ├──────┼───────────┼───────────┼───────────┼───┤
-//    │ ( │ LALT_T(left) │ LCTL_T(down) │ LSFT_T(rght) │ LGUI_T()) │                              │ lgui │ LSFT_T(4) │ LCTL_T(5) │ LALT_T(6) │ \ │
-//    ├───┼──────────────┼──────────────┼──────────────┼───────────┤                              ├──────┼───────────┼───────────┼───────────┼───┤
-//    │ [ │      ]       │      #       │      {       │     }     │                              │  0   │     1     │     2     │     3     │ = │
-//    └───┴──────────────┴──────────────┴──────────────┼───────────┼─────┐   ┌────────────────────┼──────┼───────────┴───────────┴───────────┴───┘
-//                                                     │           │     │   │ OSL(_ESCAPE_HATCH) │      │
-//                                                     └───────────┴─────┘   └────────────────────┴──────┘
+//    ┌───┬──────────────┬──────────────┬──────────────┬───────────┐                             ┌──────┬───────────┬───────────┬───────────┬───┐
+//    │ ` │      =       │      -       │      '       │    tab    │                             │  no  │     7     │     8     │     9     │ | │
+//    ├───┼──────────────┼──────────────┼──────────────┼───────────┤                             ├──────┼───────────┼───────────┼───────────┼───┤
+//    │ ( │ LALT_T(left) │ LCTL_T(down) │ LSFT_T(rght) │ LGUI_T()) │                             │ lgui │ LSFT_T(4) │ LCTL_T(5) │ LALT_T(6) │ \ │
+//    ├───┼──────────────┼──────────────┼──────────────┼───────────┤                             ├──────┼───────────┼───────────┼───────────┼───┤
+//    │ [ │      ]       │      #       │      {       │     }     │                             │  0   │     1     │     2     │     3     │ = │
+//    └───┴──────────────┴──────────────┴──────────────┼───────────┼─────┐   ┌───────────────────┼──────┼───────────┴───────────┴───────────┴───┘
+//                                                     │           │     │   │ MO(_ESCAPE_HATCH) │      │
+//                                                     └───────────┴─────┘   └───────────────────┴──────┘
 [_SYMBOLS] = LAYOUT_split_3x5_2(
-  KC_GRAVE , KC_EQL          , KC_MINS         , KC_QUOT         , C(KC_BSLS)      ,                                    KC_NO   , KC_7         , KC_8         , KC_9         , KC_PIPE,
-  KC_LPRN  , LALT_T(KC_LEFT) , LCTL_T(KC_DOWN) , LSFT_T(KC_RGHT) , LGUI_T(KC_RPRN) ,                                    KC_LGUI , LSFT_T(KC_4) , LCTL_T(KC_5) , LALT_T(KC_6) , KC_BSLS,
-  KC_LBRC  , KC_RBRC         , KC_HASH         , KC_LCBR         , KC_RCBR         ,                                    KC_0    , KC_1         , KC_2         , KC_3         , KC_EQL ,
-                                                                   KC_TRNS         , KC_TRNS ,     OSL(_ESCAPE_HATCH) , KC_TRNS
+  KC_GRAVE , KC_EQL          , KC_MINS         , KC_QUOT         , KC_TAB          ,                                   KC_NO   , KC_7         , KC_8         , KC_9         , KC_PIPE,
+  KC_LPRN  , LALT_T(KC_LEFT) , LCTL_T(KC_DOWN) , LSFT_T(KC_RGHT) , LGUI_T(KC_RPRN) ,                                   KC_LGUI , LSFT_T(KC_4) , LCTL_T(KC_5) , LALT_T(KC_6) , KC_BSLS,
+  KC_LBRC  , KC_RBRC         , KC_HASH         , KC_LCBR         , KC_RCBR         ,                                   KC_0    , KC_1         , KC_2         , KC_3         , KC_EQL ,
+                                                                   KC_TRNS         , KC_TRNS ,     MO(_ESCAPE_HATCH) , KC_TRNS
 ),
 
-//    ┌─────┬──────┬─────┬──────┬──────┐                              ┌──────┬──────┬──────┬──────┬─────┐
-//    │  `  │  =   │  -  │  '   │ C(\) │                              │      │ btn1 │ wh_u │ btn2 │     │
-//    ├─────┼──────┼─────┼──────┼──────┤                              ├──────┼──────┼──────┼──────┼─────┤
-//    │     │ wh_l │     │ wh_r │      │                              │ ms_l │ ms_d │ ms_u │ ms_r │     │
-//    ├─────┼──────┼─────┼──────┼──────┤                              ├──────┼──────┼──────┼──────┼─────┤
-//    │  [  │  ]   │  #  │  {   │  }   │                              │      │ wh_l │ wh_d │ wh_r │     │
-//    └─────┴──────┴─────┴──────┼──────┼────────────────────┐   ┌─────┼──────┼──────┴──────┴──────┴─────┘
-//                              │      │ OSL(_ESCAPE_HATCH) │   │     │      │
-//                              └──────┴────────────────────┘   └─────┴──────┘
+//    ┌───┬──────┬─────┬──────┬─────┐                              ┌──────┬──────┬──────┬──────┬─────┐
+//    │ ` │  =   │  -  │  '   │ tab │                              │      │ btn1 │ wh_u │ btn2 │     │
+//    ├───┼──────┼─────┼──────┼─────┤                              ├──────┼──────┼──────┼──────┼─────┤
+//    │ ( │ wh_l │     │ wh_r │  )  │                              │ ms_l │ ms_d │ ms_u │ ms_r │     │
+//    ├───┼──────┼─────┼──────┼─────┤                              ├──────┼──────┼──────┼──────┼─────┤
+//    │ [ │  ]   │  #  │  {   │  }  │                              │      │ wh_l │ wh_d │ wh_r │     │
+//    └───┴──────┴─────┴──────┼─────┼────────────────────┐   ┌─────┼──────┼──────┴──────┴──────┴─────┘
+//                            │     │ OSL(_ESCAPE_HATCH) │   │     │      │
+//                            └─────┴────────────────────┘   └─────┴──────┘
 [_MOUSE] = LAYOUT_split_3x5_2(
-  KC_GRAVE , KC_EQL  , KC_MINS , KC_QUOT , C(KC_BSLS) ,                                    KC_TRNS , KC_BTN1 , KC_WH_U , KC_BTN2 , KC_TRNS,
-  KC_TRNS  , KC_WH_L , KC_TRNS , KC_WH_R , KC_TRNS    ,                                    KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_TRNS,
-  KC_LBRC  , KC_RBRC , KC_HASH , KC_LCBR , KC_RCBR    ,                                    KC_TRNS , KC_WH_L , KC_WH_D , KC_WH_R , KC_TRNS,
-                                           KC_TRNS    , OSL(_ESCAPE_HATCH) ,     KC_TRNS , KC_TRNS
+  KC_GRAVE , KC_EQL  , KC_MINS , KC_QUOT , KC_TAB  ,                                    KC_TRNS , KC_BTN1 , KC_WH_U , KC_BTN2 , KC_TRNS,
+  KC_LPRN  , KC_WH_L , KC_TRNS , KC_WH_R , KC_RPRN ,                                    KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_TRNS,
+  KC_LBRC  , KC_RBRC , KC_HASH , KC_LCBR , KC_RCBR ,                                    KC_TRNS , KC_WH_L , KC_WH_D , KC_WH_R , KC_TRNS,
+                                           KC_TRNS , OSL(_ESCAPE_HATCH) ,     KC_TRNS , KC_TRNS
 ),
 
-//    ┌─────┬─────┬─────┬─────┬─────┐               ┌─────┬─────────┬─────────┬─────────┬──────────┐
-//    │     │     │     │     │     │               │ del │ LCA(f7) │ LCA(f8) │ LCA(f9) │ LCA(f10) │
-//    ├─────┼─────┼─────┼─────┼─────┤               ├─────┼─────────┼─────────┼─────────┼──────────┤
-//    │     │     │     │     │     │               │ f2  │ LCA(f4) │ LCA(f5) │ LCA(f6) │ LCA(f11) │
-//    ├─────┼─────┼─────┼─────┼─────┤               ├─────┼─────────┼─────────┼─────────┼──────────┤
-//    │     │     │     │     │     │               │ f12 │ LCA(f1) │ LCA(f2) │ LCA(f3) │ LCA(f12) │
-//    └─────┴─────┴─────┴─────┼─────┼─────┐   ┌─────┼─────┼─────────┴─────────┴─────────┴──────────┘
-//                            │     │     │   │     │     │
-//                            └─────┴─────┘   └─────┴─────┘
+//    ┌─────┬──────┬──────┬──────┬─────┐               ┌─────┬────┬────┬────┬─────┐
+//    │     │      │  up  │      │     │               │ del │ f7 │ f8 │ f9 │ f10 │
+//    ├─────┼──────┼──────┼──────┼─────┤               ├─────┼────┼────┼────┼─────┤
+//    │     │ left │ down │ rght │     │               │     │ f4 │ f5 │ f6 │ f11 │
+//    ├─────┼──────┼──────┼──────┼─────┤               ├─────┼────┼────┼────┼─────┤
+//    │     │      │      │      │     │               │     │ f1 │ f2 │ f3 │ f12 │
+//    └─────┴──────┴──────┴──────┼─────┼─────┐   ┌─────┼─────┼────┴────┴────┴─────┘
+//                               │     │     │   │     │     │
+//                               └─────┴─────┘   └─────┴─────┘
 [_ESCAPE_HATCH] = LAYOUT_split_3x5_2(
-  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                         KC_DEL  , LCA(KC_F7) , LCA(KC_F8) , LCA(KC_F9) , LCA(KC_F10),
-  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                         KC_F2   , LCA(KC_F4) , LCA(KC_F5) , LCA(KC_F6) , LCA(KC_F11),
-  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                         KC_F12  , LCA(KC_F1) , LCA(KC_F2) , LCA(KC_F3) , LCA(KC_F12),
-                                          KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS
+  KC_TRNS , KC_TRNS , KC_UP   , KC_TRNS  , KC_TRNS ,                         KC_DEL  , KC_F7 , KC_F8 , KC_F9 , KC_F10,
+  KC_TRNS , KC_LEFT , KC_DOWN , KC_RIGHT , KC_TRNS ,                         KC_TRNS , KC_F4 , KC_F5 , KC_F6 , KC_F11,
+  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS  , KC_TRNS ,                         KC_TRNS , KC_F1 , KC_F2 , KC_F3 , KC_F12,
+                                           KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS
 )
 };
 
