d16ba3898f883f6d06d80c90659cb69874fae835
alex <alexnlarsen@gmail.com>
2024-02-04 09:04:40 +0100

    Make function layer more accessible

--- keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
@@ -4,7 +4,7 @@ enum layer_names {
     _HOMEROW,
     _SYMBOLS,
     _MOUSE,
-    _ESCAPE_HATCH,
+    _FUNCTION,
 };
 
 enum combos {
@@ -36,21 +36,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q     , KC_W         , KC_E         , KC_R         , KC_T         ,                 KC_Y                   , KC_U         , KC_I         , KC_O         , KC_P    ,
   KC_A     , LALT_T(KC_S) , LCTL_T(KC_D) , LSFT_T(KC_F) , LGUI_T(KC_G) ,                 LGUI_T(KC_H)           , RSFT_T(KC_J) , LCTL_T(KC_K) , LALT_T(KC_L) , KC_SCLN ,
   KC_Z     , KC_X         , KC_C         , KC_V         , KC_B         ,                 KC_N                   , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH ,
-                                           KC_BSPC      , LT(MO(_SYMBOLS), KC_TAB) ,     LT(MO(_MOUSE), KC_ENT) , KC_SPC
+                                           KC_BSPC      , LT(MO(_SYMBOLS), KC_TAB) ,     LT(MO(_MOUSE), KC_ENT) , LT(MO(_FUNCTION), KC_SPC)
 ),
 [_SYMBOLS] = LAYOUT_split_3x5_2(
   KC_GRAVE , KC_TILD         , KC_MINS         , KC_UNDS         , KC_ASTR         ,     KC_QUOT           , KC_7         , KC_8         , KC_9         , KC_EQL ,
   KC_LPRN  , LALT_T(KC_LEFT) , LCTL_T(KC_DOWN) , LSFT_T(KC_RGHT) , KC_RPRN         ,     KC_LGUI           , RSFT_T(KC_4) , LCTL_T(KC_5) , LALT_T(KC_6) , KC_BSLS,
   KC_LBRC  , KC_RBRC         , KC_HASH         , KC_LCBR         , KC_RCBR         ,     KC_0              , KC_1         , KC_2         , KC_3         , KC_EQL ,
-                                                 KC_DEL          , KC_TRNS         ,     MO(_ESCAPE_HATCH) , KC_TRNS
+                                                 KC_DEL          , KC_TRNS         ,     MO(_FUNCTION) , MO(_ESCAPE_HATCH)
 ),
 [_MOUSE] = LAYOUT_split_3x5_2(
-  KC_GRAVE , KC_EQL  , KC_UP   , KC_QUOT  , KC_TAB  ,    KC_TRNS , KC_BTN1 , KC_WH_U , KC_BTN2 , KC_TRNS,
+  KC_GRAVE , KC_EQL  , KC_UP   , KC_QUOT  , KC_TAB  ,    KC_WBAK , KC_BTN1 , KC_WH_U , KC_BTN2 , KC_WFWD,
   KC_LPRN  , KC_LEFT , KC_DOWN , KC_RIGHT , KC_RPRN ,    KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_TRNS,
   KC_LBRC  , KC_RBRC , KC_HASH , KC_LCBR  , KC_RCBR ,    KC_TRNS , KC_WH_L , KC_WH_D , KC_WH_R , KC_TRNS,
                                  KC_TRNS  , KC_TRNS ,    KC_TRNS , KC_TRNS
 ),
-[_ESCAPE_HATCH] = LAYOUT_split_3x5_2(
+[_FUNCTION] = LAYOUT_split_3x5_2(
   KC_TRNS , KC_TRNS , KC_UP   , KC_TRNS  , KC_TRNS ,     KC_DEL  , KC_F7 , KC_F8 , KC_F9 , KC_F10,
   KC_TRNS , KC_LEFT , KC_DOWN , KC_RIGHT , KC_TRNS ,     KC_TRNS , KC_F4 , KC_F5 , KC_F6 , KC_F11,
   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS  , KC_TRNS ,     KC_TRNS , KC_F1 , KC_F2 , KC_F3 , KC_F12,
