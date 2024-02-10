d7dd42c8e7bf4ec8163b8d875eeadd89516e9fd5
alex <alexnlarsen@gmail.com>
2024-02-10 19:08:20 +0100

    Fix numbers

--- keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
@@ -22,7 +22,7 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
 };
 
-const uint16_t PROGMEM jk_combo[]       = {RSFT_T(KC_J), LCTL_T(KC_K), COMBO_END};
+const uint16_t PROGMEM jk_combo[]       = {RSFT_T(KC_J), RCTL_T(KC_K), COMBO_END};
 const uint16_t PROGMEM fj_combo[]       = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
 
 combo_t key_combos[] = {
@@ -34,13 +34,13 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_HOMEROW] = LAYOUT_split_3x5_2(
   KC_Q       , KC_W              , KC_E              , KC_R               , KC_T                     ,     KC_Y                   , KC_U             , KC_I          , KC_O          , KC_P    ,
-  KC_A       , LALT_T(KC_S)      , LCTL_T(KC_D)      , LSFT_T(KC_F)       , LGUI_T(KC_G)             ,     LGUI_T(KC_H)           , RSFT_T(KC_J)     , LCTL_T(KC_K)  , LALT_T(KC_L)  , KC_SCLN ,
+  KC_A       , LALT_T(KC_S)      , LCTL_T(KC_D)      , LSFT_T(KC_F)       , LGUI_T(KC_G)             ,     RGUI_T(KC_H)           , RSFT_T(KC_J)     , RCTL_T(KC_K)  , RALT_T(KC_L)  , KC_SCLN ,
   KC_Z       , KC_X              , KC_C              , KC_V               , KC_B                     ,     KC_N                   , KC_M             , KC_COMM       , KC_DOT        , KC_SLSH ,
                                                        KC_BSPC            , LT(MO(_SYMBOLS), KC_TAB) ,     LT(MO(_MOUSE), KC_ENT) , LT(MO(_FUNCTION) , KC_SPC)
 ),
 [_SYMBOLS] = LAYOUT_split_3x5_2(
   KC_GRAVE   , KC_TILD           , KC_MINS           , KC_UNDS            , KC_ASTR                  ,     KC_QUOT                , KC_7             , KC_8          , KC_9          , KC_EQL ,
-  KC_LPRN    , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)   , LSFT_T(KC_RGHT)    , KC_RPRN                  ,     KC_LGUI                , RSFT_T(KC_4)     , LCTL_T(KC_5)  , LALT_T(KC_6)  , KC_BSLS,
+  KC_LPRN    , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)   , LSFT_T(KC_RGHT)    , KC_RPRN                  ,     KC_LGUI                , RSFT_T(KC_4)     , RCTL_T(KC_5)  , RALT_T(KC_6)  , KC_BSLS,
   KC_LBRC    , KC_RBRC           , KC_HASH           , KC_LCBR            , KC_RCBR                  ,     KC_0                   , KC_1             , KC_2          , KC_3          , KC_EQL ,
                                                        KC_DEL             , KC_TRNS                  ,     MO(_FUNCTION)          , MO(_FUNCTION)
 ),
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_FUNCTION] = LAYOUT_split_3x5_2(
   ALGR(KC_W) , RGB_TOG           , KC_UP             , RGB_MOD            , RGB_HUI                  ,     KC_DEL                 , KC_F7            , KC_F8         , KC_F9         , KC_F10,
-  ALGR(KC_L) , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)   , LSFT_T(KC_RIGHT)   , KC_LGUI                  ,     KC_LGUI                , RSFT_T(KC_F4)    , LCTL_T(KC_F5) , LALT_T(KC_F6) , KC_F11,
+  ALGR(KC_L) , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)   , LSFT_T(KC_RIGHT)   , KC_LGUI                  ,     KC_LGUI                , RSFT_T(KC_F4)    , RCTL_T(KC_F5) , RALT_T(KC_F6) , KC_F11,
   ALGR(KC_Z) , KC_TRNS           , KC_TRNS           , KC_TRNS            , KC_TRNS                  ,     KC_TRNS                , KC_F1            , KC_F2         , KC_F3         , KC_F12,
                                                        KC_TRNS            , KC_TRNS                  ,     KC_TRNS                , KC_TRNS
 )};
