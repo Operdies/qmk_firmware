368d7fce8a2497867db898c88137f4d6171c3e87
alex <alexnlarsen@gmail.com>
2024-02-11 10:55:59 +0100

    Add new cool layer
    Try using classic 1-9,0 on a layer
    
    Disable _S2. Maybe later?

--- flash.sh
+++ flash.sh
@@ -3,11 +3,11 @@
 set -e
 
 make splitkb/aurora/sweep/rev1:other
-for i in $(seq 1 10); do
+for i in $(seq 1 100); do
     if udisksctl mount -b /dev/disk/by-id/usb-RPI_RP2_E0C9125B0D9B-0:0-part1 ; then
         break
     fi
-    echo "Mount fail $i / 10"
+    echo "Mount fail $i / 100"
     sleep 1
 done
 
--- keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
@@ -2,9 +2,10 @@
 
 enum layer_names {
     _HOMEROW,
-    _SYMBOLS,
+    _S1,
     _MOUSE,
     _FUNCTION,
+    _S2,
 };
 
 enum combos {
@@ -33,29 +34,37 @@ combo_t key_combos[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_HOMEROW] = LAYOUT_split_3x5_2(
-  KC_Q       , KC_W              , KC_E              , KC_R               , KC_T                     ,     KC_Y                   , KC_U             , KC_I          , KC_O          , KC_P    ,
-  KC_A       , LALT_T(KC_S)      , LCTL_T(KC_D)      , LSFT_T(KC_F)       , LGUI_T(KC_G)             ,     RGUI_T(KC_H)           , RSFT_T(KC_J)     , RCTL_T(KC_K)  , RALT_T(KC_L)  , KC_SCLN ,
-  KC_Z       , KC_X              , KC_C              , KC_V               , KC_B                     ,     KC_N                   , KC_M             , KC_COMM       , KC_DOT        , KC_SLSH ,
-                                                       KC_BSPC            , LT(MO(_SYMBOLS), KC_TAB) ,     LT(MO(_MOUSE), KC_ENT) , LT(MO(_FUNCTION) , KC_SPC)
+  KC_Q       , KC_W              , KC_E                , KC_R                   , KC_T                     ,     KC_Y                   , KC_U             , KC_I          , KC_O          , KC_P     ,
+  KC_A       , LALT_T(KC_S)      , LCTL_T(KC_D)        , LSFT_T(KC_F)           , LGUI_T(KC_G)             ,     RGUI_T(KC_H)           , RSFT_T(KC_J)     , RCTL_T(KC_K)  , RALT_T(KC_L)  , KC_SCLN  ,
+  KC_Z       , KC_X              , KC_C                , KC_V                   , KC_B                     ,     KC_N                   , KC_M             , KC_COMM       , KC_DOT        , KC_SLSH  ,
+                                                         KC_BSPC                , LT(MO(_S1), KC_TAB)      ,     LT(MO(_MOUSE), KC_ENT) , LT(MO(_FUNCTION) , KC_SPC)
+                                                      // LT(MO(_S2), KC_BSPC)
 ),
-[_SYMBOLS] = LAYOUT_split_3x5_2(
-  KC_GRAVE   , KC_TILD           , KC_MINS           , KC_UNDS            , KC_ASTR                  ,     KC_QUOT                , KC_7             , KC_8          , KC_9          , KC_EQL ,
-  KC_LPRN    , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)   , LSFT_T(KC_RGHT)    , KC_RPRN                  ,     KC_LGUI                , RSFT_T(KC_4)     , RCTL_T(KC_5)  , RALT_T(KC_6)  , KC_BSLS,
-  KC_LBRC    , KC_RBRC           , KC_HASH           , KC_LCBR            , KC_RCBR                  ,     KC_0                   , KC_1             , KC_2          , KC_3          , KC_EQL ,
-                                                       KC_DEL             , KC_TRNS                  ,     MO(_FUNCTION)          , MO(_FUNCTION)
+[_S1] = LAYOUT_split_3x5_2(
+  KC_GRAVE   , KC_TILD           , KC_MINS             , KC_LBRC                , KC_RBRC                  ,     KC_QUOT                , KC_7             , KC_8          , KC_9          , KC_EQL   ,
+  KC_LPRN    , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)     , LSFT_T(KC_RGHT)        , KC_RPRN                  ,     KC_LGUI                , RSFT_T(KC_4)     , RCTL_T(KC_5)  , RALT_T(KC_6)  , KC_BSLS  ,
+  KC_LBRC    , KC_RBRC           , KC_HASH             , KC_LCBR                , KC_RCBR                  ,     KC_0                   , KC_1             , KC_2          , KC_3          , KC_PIPE  ,
+                                                         KC_DEL                 , KC_TRNS                  ,     KC_TRNS                , KC_TRNS
+),
+[_S2] = LAYOUT_split_3x5_2(
+  KC_TRNS    , KC_TRNS           , KC_TRNS             , KC_TRNS                , KC_TRNS                  ,     KC_TRNS                , KC_TRNS          , KC_TRNS       , KC_TRNS       , KC_TRNS  ,
+  KC_TRNS    , KC_TRNS           , KC_TRNS             , KC_TRNS                , KC_TRNS                  ,     KC_TRNS                , KC_TRNS          , KC_TRNS       , KC_TRNS       , KC_TRNS  ,
+  KC_TRNS    , KC_TRNS           , KC_TRNS             , KC_TRNS                , KC_TRNS                  ,     KC_TRNS                , KC_TRNS          , KC_TRNS       , KC_TRNS       , KC_TRNS  ,
+                                                         KC_TRNS                , KC_TRNS                  ,     KC_TRNS                , KC_TRNS
 ),
 [_MOUSE] = LAYOUT_split_3x5_2(
-  KC_LBRC    , KC_TILD           , KC_ASTR           , KC_AMPR            , KC_RBRC                  ,     KC_BTN4                , KC_BTN1          , KC_WH_U       , KC_BTN2       , KC_BTN5,
-  KC_LPRN    , LALT_T(KC_CIRC)   , LCTL_T(KC_PERC)   , LSFT_T(KC_DLR)     , LGUI_T(KC_RPRN)          ,     KC_MS_L                , KC_MS_D          , KC_MS_U       , KC_MS_R       , KC_BTN3,
-  KC_LCBR    , KC_HASH           , KC_AT             , KC_EXLM            , KC_RCBR                  ,     KC_TRNS                , KC_WH_L          , KC_WH_D       , KC_WH_R       , KC_TRNS,
-                                                       KC_TRNS            , KC_TRNS                  ,     KC_TRNS                , KC_TRNS
+  KC_LBRC    , KC_TILD           , KC_ASTR             , KC_AMPR                , KC_RBRC                  ,     KC_BTN4                , KC_BTN1          , KC_WH_U       , KC_BTN2       , KC_BTN5  ,
+  KC_LPRN    , LALT_T(KC_CIRC)   , LCTL_T(KC_PERC)     , LSFT_T(KC_DLR)         , LGUI_T(KC_RPRN)          ,     KC_MS_L                , KC_MS_D          , KC_MS_U       , KC_MS_R       , KC_BTN3  ,
+  KC_LCBR    , KC_HASH           , KC_AT               , KC_EXLM                , KC_RCBR                  ,     KC_TRNS                , KC_WH_L          , KC_WH_D       , KC_WH_R       , KC_TRNS  ,
+                                                         KC_TRNS                , KC_TRNS                  ,     KC_TRNS                , KC_TRNS
 ),
 [_FUNCTION] = LAYOUT_split_3x5_2(
-  ALGR(KC_W) , RGB_TOG           , KC_UP             , RGB_MOD            , RGB_HUI                  ,     KC_DEL                 , KC_F7            , KC_F8         , KC_F9         , KC_F10,
-  ALGR(KC_L) , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)   , LSFT_T(KC_RIGHT)   , KC_LGUI                  ,     KC_LGUI                , RSFT_T(KC_F4)    , RCTL_T(KC_F5) , RALT_T(KC_F6) , KC_F11,
-  ALGR(KC_Z) , KC_TRNS           , KC_TRNS           , KC_TRNS            , RGB_SAI                  ,     KC_TRNS                , KC_F1            , KC_F2         , KC_F3         , KC_F12,
-                                                       KC_TRNS            , RGB_VAI                  ,     KC_TRNS                , KC_TRNS
-)};
+  ALGR(KC_W) , RGB_TOG           , KC_UP               , RGB_MOD                , RGB_HUI                  ,     KC_DEL                 , KC_F7            , KC_F8         , KC_F9         , KC_F10   ,
+  ALGR(KC_L) , LALT_T(KC_LEFT)   , LCTL_T(KC_DOWN)     , LSFT_T(KC_RIGHT)       , KC_LGUI                  ,     KC_LGUI                , RSFT_T(KC_F4)    , RCTL_T(KC_F5) , RALT_T(KC_F6) , KC_F11   ,
+  ALGR(KC_Z) , KC_TRNS           , KC_TRNS             , KC_TRNS                , RGB_SAI                  ,     KC_TRNS                , KC_F1            , KC_F2         , KC_F3         , KC_F12   ,
+                                                         KC_TRNS                , RGB_VAI                  ,     KC_TRNS                , KC_TRNS
+),
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
