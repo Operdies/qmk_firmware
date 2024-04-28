877fae00a967abb6cd5f25521d9be5189dc0b8f1
alex <alexnlarsen@gmail.com>
2024-04-28 06:04:57 +0200

    Disable hold function on comma and dot

--- keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/other/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_HOMEROW] = LAYOUT_split_3x5_2(
   KC_Q       , KC_W              , KC_E                , KC_R                   , KC_T                     ,     KC_Y                   , KC_U             , KC_I          , KC_O          , KC_P      ,
   KC_A       , LALT_T(KC_S)      , LCTL_T(KC_D)        , LSFT_T(KC_F)           , LGUI_T(KC_G)             ,     LGUI_T(KC_H)           , LSFT_T(KC_J)     , LCTL_T(KC_K)  , LALT_T(KC_L)  , KC_SCLN  ,
-  KC_Z       , KC_X              , KC_C                , KC_V                   , KC_B                     ,     KC_N                   , KC_M             , CK_COMM       , CK_DOT        , KC_SLSH    ,
+  KC_Z       , KC_X              , KC_C                , KC_V                   , KC_B                     ,     KC_N                   , KC_M             , KC_COMM       , KC_DOT        , KC_SLSH    ,
                                                          KC_BSPC                , MO(_S1)                  ,     LT(_MOUSE, KC_ENT)     , LT(_FUNCTION , KC_SPC)
 ),
 [_S1] = LAYOUT_split_3x5_2( // TODO: The lhs of this layer is kinda wacky. Never use arrow keys or hash, trying to move away from the braces
